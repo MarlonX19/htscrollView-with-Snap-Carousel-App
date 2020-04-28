1f47add57d61865f784283e822ba55fad661bb23
Jhonatan <jlopes@q9.com.br>
2020-04-28 13:08:18 -0300

    Added modal component

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import {Text, View, SafeAreaView} from 'react-native';
+import { Text, Modal, View, TouchableHighlight, StyleSheet, TouchableOpacity, SafeAreaView } from 'react-native';
 import Carousel from 'react-native-snap-carousel';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      modalVisible: false,
       activeIndex: 0,
       carouselItems: [
         {
@@ -30,30 +31,36 @@ export default class App extends React.Component {
         },
       ],
     };
+
+    this._renderItem = this._renderItem.bind(this);
   }
 
-  _renderItem({item, index}) {
+  _renderItem({ item, index }) {
     return (
-      <View
-        style={{
-          backgroundColor: 'floralwhite',
-          borderRadius: 5,
-          height: 250,
-          padding: 50,
-          marginLeft: 25,
-          marginRight: 25,
-        }}>
-        <Text style={{fontSize: 30}}>{item.title}</Text>
-        <Text>{item.text}</Text>
-      </View>
+      <TouchableOpacity
+        onPress={() => this.setState({ modalVisible: true })}
+      >
+        <View
+          style={{
+            backgroundColor: 'floralwhite',
+            borderRadius: 5,
+            height: 250,
+            padding: 50,
+            marginLeft: 25,
+            marginRight: 25,
+          }}>
+          <Text style={{ fontSize: 30 }}>{item.title}</Text>
+          <Text>{item.text}</Text>
+        </View>
+      </TouchableOpacity>
     );
   }
 
   render() {
     return (
       <SafeAreaView
-        style={{flex: 1, backgroundColor: 'rebeccapurple', paddingTop: 50}}>
-        <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center'}}>
+        style={{ flex: 1, backgroundColor: 'rebeccapurple', paddingTop: 50 }}>
+        <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
           <Carousel
             layout={'default'}
             ref={(ref) => (this.carousel = ref)}
@@ -61,10 +68,66 @@ export default class App extends React.Component {
             sliderWidth={300}
             itemWidth={300}
             renderItem={this._renderItem}
-            onSnapToItem={(index) => this.setState({activeIndex: index})}
+            onSnapToItem={(index) => this.setState({ activeIndex: index })}
           />
         </View>
+
+
+        <Modal
+          animationType="fade"
+          transparent={true}
+          visible={this.state.modalVisible}
+          hardwareAccelerated={true}
+          onRequestClose={() => {
+            Alert.alert('Modal has been closed.');
+          }}>
+          <View style={styles.modalView}>
+            <View style={styles.modalPhoto}>
+              <Text style={styles.txt}>{this.state.carouselItems[this.state.activeIndex].title}</Text>
+              <TouchableHighlight
+                onPress={() =>
+                  this.setState({ modalVisible: false })
+                }>
+                <Text>fechar</Text>
+              </TouchableHighlight>
+            </View>
+          </View>
+        </Modal>
       </SafeAreaView>
     );
   }
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  modalView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+
+  modalPhoto: {
+    width: 170,
+    height: 170,
+    borderRadius: 5,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+      
+  txt: {
+    color: 'blue',
+    fontSize: 32,
+    fontWeight: 'bold',
+    marginBottom: 14
+  }
+
+
+});
